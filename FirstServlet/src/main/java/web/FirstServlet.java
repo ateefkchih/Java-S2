aa88c345fe2d446a2043f1707b05f46e7eef1694
ateef <ateefkchih@gmail.com>
2024-03-02 10:34:12 +0100

    TP

---
+++ FirstServlet/src/main/java/web/FirstServlet.java
@@ -0,0 +1,41 @@
+package web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public FirstServlet() {
+        super();
+    }
+
+
+		protected void doGet(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+				response.setContentType("text/html; charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				out.println("<!DOCTYPE html>");
+				out.println("<html>");
+				out.println("<head>");
+				out.println("<title> FirstServelet</title>");
+				out.println("</head>");
+				out.println("<body>");
+				out.println("It's my First Servelet ");
+				out.println("</body>");
+				out.println("</html>");	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
